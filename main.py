7f9592b43b5e0a58844c0434231b2acbd5310fef
SamPCS <svogel@pcsknox.com>
2025-03-10 14:18:36 -0400

    New Testing and .gitignore

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ from spell import *
 from player import *
 
 def main():
+
+    print("Creating famed Necromancer, Samuel ShadowSong!")
+    death_student = Player("Samuel ShadowSong", "Death", 164)
+    print("Retrieving a Master Necromancer Spellbook...")
+    death_student.fill_spellbook()
+    print("The following spells are in the spellbook:")
+    death_student.check_spellbook()
     curr_stack = Magic_Stack()
+    print()
 
     death_blade = Spell("Deathblade", "Death", 0, 1.00, "Charm", 7, "Self", 0.35, "Applies a 35% Death Damage or Steal Charm")
     dark_sprite = Spell("Dark Sprite", "Death", 1, 0.85, "Attack", 1, "Enemy", 0, "Deals 65-105 Death Damage", 65, 105)
@@ -13,21 +21,13 @@ def main():
     print("Casting a Lifeblade...")
     curr_stack.add_spell(life_blade)
 
-
-
     print("Casting a Dark Sprite!")
     min, max = curr_stack.cast_stack(dark_sprite)
     print(f"You will deal between {min} and {max} to the target.")
+    print()
 
     print("What is left on the stack: ")
     curr_stack.check_stack()
-
-    print("Creating famed Necromancer, Samuel ShadowSong!")
-    death_student = Player("Samuel ShadowSong", "Death", 164)
-    print("Retrieving a Master Necromancer Spellbook...")
-    death_student.fill_spellbook()
-    print("The following spells are in the spellbook:")
-    death_student.check_spellbook()
     
 
 if __name__ == "__main__":
